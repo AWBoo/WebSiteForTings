0483e8fb7818f3ffa38897b537b59c64861721f8
AWBoo <140343993+AWBoo@users.noreply.github.com>
2025-04-04 14:08:12 +0200

    sdfg

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,28 @@
-export default defineConfig({
-    plugins: [
-      laravel({
-        input: [
-          'resources/sass/app.scss',
-          'resources/js/app.js',
-        ],
-        refresh: true,
-      }),
-      vue({
-        template: {
-          transformAssetUrls: {
-            base: null,
-            includeAbsolute: false,
-          },
+const { defineConfig } = require('vite');
+const vue = require('@vitejs/plugin-vue');
+const laravel = require('laravel-vite-plugin');
+
+module.exports = defineConfig({
+  plugins: [
+    laravel({
+      input: [
+        'resources/sass/app.scss',
+        'resources/js/app.js',
+      ],
+      refresh: true,
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
         },
-      }),
-    ],
-    resolve: {
-      alias: {
-        vue: 'vue/dist/vue.esm-bundler.js',
       },
+    }),
+  ],
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.esm-bundler.js',
     },
-    // Specify base URL for production build
-    base: process.env.NODE_ENV === 'production' ? '/build/' : '/',
+  },
 });
